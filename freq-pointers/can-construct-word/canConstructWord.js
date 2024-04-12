5602e92740aef000b73ec8390d7628a78fc8405e
Jacob <jacobrashid99@gmail.com>
2024-04-12 15:24:49 -0700

    Implemented canConstructWord

--- freq-pointers/can-construct-word/canConstructWord.js
+++ freq-pointers/can-construct-word/canConstructWord.js
@@ -1,5 +1,27 @@
+/** Takes in word and letters as lowercase strings with no spaces or punctuation, 
+ * returns true if word can be made from letters, and false if it can't. 
+ */
 function canConstructWord(word, letters) {
-    return false;
+    const wordFreqs = getFreqs(word);
+    const letterFreqs = getFreqs(letters);
+    
+    for(let letter in wordFreqs){ 
+        if(!letterFreqs[letter]) return false;
+        if(!(letterFreqs[letter] >= wordFreqs[letter])) return false;
+    }
+    
+    return true;
+}
+
+/** Takes in a string 'word', returns an object with letter frequencies */
+function getFreqs(word){
+    const freqs = {};
+    for(const letter of word){
+        const currCount = freqs[letter] || 0;
+        freqs[letter] = currCount + 1;
+    }
+    
+    return freqs;
 }
 
 export { canConstructWord };
