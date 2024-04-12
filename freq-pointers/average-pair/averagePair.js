68313f09ebff8d7cbc79e75acebaf58f9938648e
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-04-12 18:50:00 -0400

    Wrote averagePair function using the multiple pointer strategy.

--- freq-pointers/average-pair/averagePair.js
+++ freq-pointers/average-pair/averagePair.js
@@ -1,4 +1,28 @@
+
+/** Takes two params: nums (a sorted array of numbers), and a num for targetAvg.
+ * Returns a boolean:
+ *    true if at least one pair's avg equals targetAvg, false if not.
+*/
+
 function averagePair(nums, targetAvg) {
+
+    if (nums.length <= 1) return false;
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const avg = (nums[left] + nums[right]) / 2;
+
+        if (avg === targetAvg) {
+            return true;
+        } else if (avg > targetAvg) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+
     return false;
 }
 
