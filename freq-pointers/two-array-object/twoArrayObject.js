7ca383b0efe73c76d2ef8b7a61b9796913ea65bc
Jacob <jacobrashid99@gmail.com>
2024-04-12 16:33:57 -0700

    implemented twoArrayObject function

--- freq-pointers/two-array-object/twoArrayObject.js
+++ freq-pointers/two-array-object/twoArrayObject.js
@@ -1,5 +1,28 @@
+
+
+// delcare an empty object   
+// for loop through keys array
+// - for each key, assign the key's value in the object to the same 
+//   index in the values array
+//   - if values run out with remaining keys, assign the key's value to null
+// return object  
+
+// ['x', 'y', 'z'], [1, 2]
+   
+
+/** Create an object using input arrays of keys and values 
+ * Input: 2 arrays, keys and values.
+*  Output: An object with assigned keys and values. If values is shorter than
+   keys, remaining values will be set to null, if keys is shorter than values
+   extra values are ignored.
+ */  
 function twoArrayObject(keys, vals) {
-    return { "": null };
+    const pairs = {};
+    for(let i = 0; i < keys.length; i++){
+        pairs[keys[i]] = vals[i] || null; 
+    }
+    
+    return pairs;
 }
 
 export { twoArrayObject };
